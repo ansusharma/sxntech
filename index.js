b64943e69901689cca58c46f5aa0d97469a78902
saman <s.saman@iitg.ac.in>
2022-07-30 23:06:01 +0530

    headers

--- index.js
+++ index.js
@@ -19,8 +19,27 @@ const corsOptions = {
     credentials: true
   };
   
-  app.options('*', cors(corsOptions));
-  app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // Middlewares
 app.use(express.json({ limit: "30mb", extended: true }));
