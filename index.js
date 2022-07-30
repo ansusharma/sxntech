76369f167031fbf9dc7148476c537076d2684639
saman <s.saman@iitg.ac.in>
2022-07-30 22:35:35 +0530

    headers

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ import usersRoute from "./routes/users.js";
 // Setup Express App
 const app = express();
 
+app.use(cors());
+
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
