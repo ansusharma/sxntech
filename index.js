51152a14a928ce8e27445dfaa2d2b602f34c683a
saman <s.saman@iitg.ac.in>
2022-07-30 22:14:13 +0530

    headers

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ app.use(
 );
 app.options('*', cors());
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 // Connect to the database
 const URI = process.env.MONGODB_URI;
 mongoose
